1148c4ec32c7e9763b5c6bc1ac64a1a66f7db405
vaeringjar <vaeringjar@peers.community>
2020-05-03 12:18:46 -0700

    init

---
+++ Makefile
@@ -0,0 +1,25 @@
+# ---------- ---------- ---------- ---------- ---------- ----------
+#
+#   med-enterprise-dash
+#
+# ---------- ---------- ---------- ---------- ---------- ----------
+
+target: help
+
+.PHONY: help
+help:
+	@echo "Usage: make [PHONY]"
+	@sed -n -e '/sed/! s/\.PHONY: //p' Makefile
+
+.PHONY: all
+all: well-formed run
+
+.PHONY: run
+run:
+	@echo "See server at the following address and port:"
+	@grep '= make_server' src/med_enterprise_dash/server.py
+	@python3 src/med_enterprise_dash/server.py
+
+.PHONY: well-formed
+well-formed:
+	@black .
---
+++ src/med_enterprise_dash/server.py
@@ -0,0 +1,38 @@
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+
+# import os, sys
+# if "med_enterprise_dash" not in sys.path:
+#     sys.path.insert(0, os.path.abspath("."))
+
+
+def home(request):
+    return Response('<body><a href="/">Home</a></body>')
+
+
+def login(request):
+    return Response("<body>Login stub...</body>")
+
+
+def logout(request):
+    return Response('<body>Go back <a href="/">home</a></body>')
+
+
+def apps(request):
+    return Response("<body>Apps stub...</body>")
+
+
+if __name__ == "__main__":
+    with Configurator() as config:
+        config.add_route("home", "/")
+        config.add_view(home, route_name="home")
+        config.add_route("logout", "/logout")
+        config.add_view(logout, route_name="logout")
+        config.add_route("login", "/login")
+        config.add_view(login, route_name="login")
+        config.add_route("apps", "/apps")
+        config.add_view(apps, route_name="apps")
+        app = config.make_wsgi_app()
+    server = make_server("0.0.0.0", 6543, app)
+    server.serve_forever()
